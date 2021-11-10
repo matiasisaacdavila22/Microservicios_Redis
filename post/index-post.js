339fec07fc7969f8ab0844d9b96e04e8f5541e3c
matias davila <mipcomputacion@gmail.com>
2021-11-10 09:23:14 -0300

    first commit2

--- post/index-post.js
+++ post/index-post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+//kfdlskf
 const config = require('../config.js');
 const post = require('./components/post/network');
 const errors = require('../network/errors');
