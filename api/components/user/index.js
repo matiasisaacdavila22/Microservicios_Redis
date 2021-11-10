2c7d8b51510d0fd64bc39e5f890135021eae438c
matias davila <mipcomputacion@gmail.com>
2021-11-10 10:44:39 -0300

    config zin.co

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,12 @@
 //const store = require('../../../store/mysql')
-const store = require('../../../store/remote-mysql')
+const config = require('../../../config');
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
 const ctrl = require('./userController');
 
 module.exports = ctrl(store);
